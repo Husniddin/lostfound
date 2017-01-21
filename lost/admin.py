7a1eb4950f695efc375ae2804905a3f4bc459592
husniddin <husniddinkamolov@gmail.com>
2017-01-22 00:23:31 +0500

    lostads admin settings.

--- lost/admin.py
+++ lost/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import LostAds
+
+class LostAdsAdmin(admin.ModelAdmin):
+	list_display=["title", "text", "email", "created_dt"]
+	class Meta:
+		model=LostAds
+	
+		
+admin.site.register(LostAds, LostAdsAdmin)
--- lost/models.py
+++ lost/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+# from datetime import date
+from django.utils import timezone
 
 class LostAds(models.Model):
-    ads_title = models.CharField(max_length=255, null=True, blank=True)
-    ads_text = models.TextField()
+    title = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    text = models.TextField()
+    created_dt = models.DateTimeField(auto_now=False, auto_now_add=True, null=True, blank=True)
+    # created_dt = models.DateTimeField(auto_now=True, auto_now_add=True, default=timezone.now(), null=True, blank=True)
+    # created_dt = models.DateField(auto_now=False, auto_now_add=True, default=date.today(), null=True, blank=True)
+
+    def __str__(self):
+    	return self.title
