5db9d05ebfbfbe5de68b1e9d6c2fadb4d798d792
husniddin <husniddinkamolov@gmail.com>
2017-01-23 21:37:54 +0500

    Bootstrap files.

--- lost/views.py
+++ lost/views.py
@@ -8,57 +8,57 @@ from .forms import LostAdsForm, ContactForm
 
 from django.core.mail import send_mail
 
+
 def index(request):
     return HttpResponse("Hello world from lost")
 
 
 def home(request):
-	title = "Hello world %s" %(request.user)
-	form = LostAdsForm(request.POST or None)
+    title = "Hello world %s" % (request.user)
+    form = LostAdsForm(request.POST or None)
 
-	context = {
-		"title": title,
-		"form": form,
-	}
+    context = {
+        "title": title,
+        "form": form,
+    }
 
-	if request.method == 'POST':
-		print(request.POST)
+    if request.method == 'POST':
+        print(request.POST)
 
+    if form.is_valid():
 
+        instance = form.save(commit=False)
 
-	if form.is_valid():
+        # Validatsiyadan o'tadi.
+        email = form.cleaned_data.get('email')
+        if not email:
+            email = "myemail@gmail.com"
+            instance.email = email
 
-		instance = form.save(commit=False)
+        if not instance.title:
+            instance.title = 'Hello world 8888'
 
-		# Validatsiyadan o'tadi.
-		email = form.cleaned_data.get('email')
-		if not email:
-			email = "myemail@gmail.com"
-			instance.email = email
+        instance.save()
+        context = {
+            "title": 'Thank you',
+        }
 
-		if not instance.title:
-			instance.title = 'Hello world 8888'
-
-		instance.save()
-		context = {
-			"title": 'Thank you',
-		}
-
-	return render(request, 'lost/home.html', context)
+    # return render(request, 'lost/home.html', context)
+    return render(request, 'lost/main.html', context)
 
 
 def contact(request):
 
-	form = ContactForm(request.POST or None)
+    form = ContactForm(request.POST or None)
 
-	if form.is_valid():
-		email = form.cleaned_data.get('email')
-		message = form.cleaned_data.get('message')
-		full_name = form.cleaned_data.get('full_name')
-		# send_mail('Site contact form', message, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER], fail_silently=True)
+    if form.is_valid():
+        email = form.cleaned_data.get('email')
+        message = form.cleaned_data.get('message')
+        full_name = form.cleaned_data.get('full_name')
+        # send_mail('Site contact form', message, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER], fail_silently=True)
 
-	context = {
-		"form": form
-	}
+    context = {
+        "form": form
+    }
 
-	return render(request, 'lost/contact.html', context)
+    return render(request, 'lost/contact.html', context)
